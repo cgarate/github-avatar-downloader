d61b2149e00d456819375e27bfa5c4d6a1be3fe2
Carlos Garate <carlos.m.garate@gmail.com>
2017-04-10 21:51:11 +0000

    Added request call to the github API with the API parameters needed to be able to connect.

--- download_avatars.js
+++ download_avatars.js
@@ -2,3 +2,37 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+var GITHUB_USER = "cgarate";
+var GITHUB_TOKEN = "a39fe022b7ae6391b7869a740e672aaae22c62d5"
+
+getRepoContributors = function (repoOwner, repoName, cb) {
+  //var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var options = {
+    //url: 'https://api.github.com/repos/request/request',
+    url: `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  request(options, function(err, response, body) {
+    if (err) throw err;
+    //console.log('Response Status Code:', response.statusCode);
+
+    //if (response.statusCode === 200) {
+      console.log(cb(body));
+    //}
+
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
+// module.exports = {
+//   getContributors: getContributors
+
+// }
