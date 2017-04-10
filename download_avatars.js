7d2c9caa580b716f78920a5d40c4f6d4a81a73ce
Carlos Garate <carlos.m.garate@gmail.com>
2017-04-10 22:29:08 +0000

    Added the download and save to local folder functionality for the avatar list.

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,13 @@
 
 var request = require('request');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 var GITHUB_USER = "cgarate";
 var GITHUB_TOKEN = "a39fe022b7ae6391b7869a740e672aaae22c62d5"
 
-getRepoContributors = function (repoOwner, repoName, cb) {
+var getRepoContributors = function (repoOwner, repoName, cb) {
   //var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   var options = {
     //url: 'https://api.github.com/repos/request/request',
@@ -27,10 +28,24 @@ getRepoContributors = function (repoOwner, repoName, cb) {
   });
 }
 
+function downloadImageByURL(url, filePath) {
+
+  request.get(url)
+       .on('error', function (err) {
+         throw err;
+       })
+       .on('response', function (response) {
+         console.log('Response Status Code: ', response.statusCode);
+       })
+       .pipe(fs.createWriteStream(filePath));
+}
+
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log(result);
+  //console.log(result);
   for (user in result) {
-    console.log(result[user].avatar_url);
+    filePath = "avatars/" + result[user].login + ".png" ;
+    downloadImageByURL(result[user].avatar_url, filePath);
+    //console.log(result[user].avatar_url, filePath);
   }
 });
 
